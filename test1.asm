d9b7fcb8f8352d691b44f78c872dbfb92b25fca8
Edu <eduardo.sanchezsarsa@gmail.com>
2025-04-23 10:18:10 +0200

    Comentario

--- test1.asm
+++ test1.asm
@@ -1,9 +1,16 @@
+#Cargamos en el set 10, vía 0 el primer bloque.
+#Luego guardaremos el valor 0 en la primera palabra del bloque de MD y MC.
+#Teniendo un Write Hit. 
 lw r1,96(r0)
 sw r2,96(r0)
 
+#Provocamos un Write miss en el set 10.
+#Como no está cargado, traerá el bloque y escribirá en la primera palabra un 0.
+#Luego tendremos un Read Hit.
 sw r2,160(r0)
 lw r3,160(r0)
 
+#Cargamos en el set 11, vía 0 un bloque para luego hacer read hits, a las palabras 01 y 10 de dicho bloque.
 lw r4,112(r0)
 lw r5,116(r0)
 lw r6,120(r0)
